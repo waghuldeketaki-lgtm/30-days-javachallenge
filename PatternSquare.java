566ff3f1369b923d204560bdcee0f08310427c38
waghuldeketaki-lgtm <waghuldeketaki@gmail.com>
2026-03-25 21:41:55 +0530

    Day 8 - Basic star patterns (square, triangle, reverse)

---
+++ PatternSquare.java
@@ -0,0 +1,11 @@
+public class PatternSquare {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {       // rows
+            for (int j = 1; j <= 5; j++) {   // columns
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ PatternTriangle.java
@@ -0,0 +1,11 @@
+public class PatternTriangle {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {     // rows
+            for (int j = 1; j <= i; j++) { // columns
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
