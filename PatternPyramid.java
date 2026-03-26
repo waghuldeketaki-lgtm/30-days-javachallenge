e073c8669be471464eae3ec40ffc2f3284386aa3
waghuldeketaki-lgtm <waghuldeketaki@gmail.com>
2026-03-27 00:09:15 +0530

    Day 9 - Pyramid and Inverted Pyramid Patterns

---
+++ PatternPyramid.java
@@ -0,0 +1,21 @@
+public class PatternPyramid {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            // Spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
